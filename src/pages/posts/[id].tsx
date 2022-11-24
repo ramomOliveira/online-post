3dad41f928f8a2447c66d1d0562be54ea22e6808
Ramom Oliveira <ra-mom2000@hotmail.com>
2022-11-24 14:46:44 -0300

    post detail created

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,23 +1,41 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import api from '../../../lib/api';
 import Header from '../../components/Header';
 import { WrapperContent } from '../../styles/global';
 import { Title, Subtitle, Content } from '../../styles/post/style.post';
 
+interface PostProps {
+  id: number;
+  title: string;
+  body: string;
+}
+
 export default function Post() {
+  const router = useRouter();
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const id = router.query.id;
+  const selectedPost = posts.find(item => item?.id === Number(id));
+
+  useEffect(() => {
+    api.get('/posts').then(response => {
+      setPosts(response.data);
+    });
+  }, [router]);
+
+  useEffect(() => {
+    api.get(`/posts/${id}/comments`).then(response => {
+      console.log(response.data);
+    });
+  }, [router]);
+
   return (
     <div>
       <Header />
       <WrapperContent>
-        <Title>
-          sunt aut facere repellat provident occaecati excepturi optio
-          reprehenderit
-        </Title>
-        <Subtitle># 1</Subtitle>
-        <Content>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore nam
-          earum ratione, quia eius quae! Inventore quo minima iure ab
-          consectetur quod optio voluptates obcaecati. Quas quod quibusdam vel
-          doloribus!
-        </Content>
+        <Title>{selectedPost?.title}</Title>
+        <Subtitle># {selectedPost?.id}</Subtitle>
+        <Content>{selectedPost?.body}</Content>
       </WrapperContent>
     </div>
   );
--- src/styles/post/style.post.ts
+++ src/styles/post/style.post.ts
@@ -1,20 +1,40 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  font-size: 48px;
-  font-weight: 900;
+  font-size: 24px;
+  font-weight: 700;
   color: #ffffff;
-  line-height: 60px;
+  line-height: 24px;
   margin-bottom: 24px;
+
+  @media (min-width: 480px) {
+    font-size: 32px;
+    line-height: 32px;
+  }
+
+  @media (min-width: 769px) {
+    font-size: 48px;
+    font-weight: 900;
+    line-height: 60px;
+  }
 `;
 
 export const Subtitle = styled.h2`
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 400;
-  margin-bottom: 32px;
+  margin-bottom: 16px;
+
+  @media (min-width: 480px) {
+    font-size: 16px;
+    margin-bottom: 32px;
+  }
 `;
 
 export const Content = styled.p`
-  font-size: 18px;
+  font-size: 16px;
   line-height: 30px;
+
+  @media (min-width: 480px) {
+    font-size: 18px;
+  }
 `;
