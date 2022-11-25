5bf985adf9988f25eab2e4d96af9e281f55d2909
Ramom Oliveira <ra-mom2000@hotmail.com>
2022-11-25 02:26:47 -0300

    home page created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,27 @@
 import Header from '../components/Header';
+import { WrapperContent } from '../styles/global';
+import { Container, ImageWomen, Aside } from '../styles/home/style';
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-
-      <div>
-        <div>
+    <WrapperContent>
+      <Container>
+        <Aside>
           <h2>Olá, bem vindo(a)</h2>
 
-          <h1>Notícias sobre o mundo da Tecnologia</h1>
+          <h1>
+            Notícias sobre o mundo da <strong>Tecnologia</strong>
+          </h1>
 
           <p>Tenha acesso a todas as publicações por R$ 9,90 mês</p>
 
           <button>Inscreva-se agora</button>
-        </div>
+        </Aside>
 
-        <div>imagem</div>
-      </div>
-    </div>
+        <ImageWomen>
+          <div />
+        </ImageWomen>
+      </Container>
+    </WrapperContent>
   );
 }
